db5b46258eb41aa6a2d9c1b55d1292f650233353
작은게이 <whya5448@gmail.com>
2019-05-01 14:59:29 +0900

    개발용 심볼릭 링크 배치

---
+++ link.cmd
@@ -0,0 +1,3 @@
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\gamedata" "%cd%\gamedata"
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoKR" "%cd%\EsoKR"
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoUI" "%cd%\EsoUI"
