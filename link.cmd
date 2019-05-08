c95418f42582eed8db3e940f3ae3cba3447d6cae
whya5448 <whya5448@gmail.com>
2019-05-08 20:27:40 +0900

    테스트 링크 배치 업데이트

--- link.cmd
+++ link.cmd
@@ -1,3 +1,8 @@
+@echo off
+rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\gamedata" /S /Q
+rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoKR" /S /Q
+rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoUI" /S /Q
+
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\gamedata" "%cd%\gamedata"
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoKR" "%cd%\EsoKR"
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoUI" "%cd%\EsoUI"
