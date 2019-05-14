d58372907de519abb914d588a5f1d3a7ca93db6f
작은게이 <whya5448@gmail.com>
2019-05-15 01:14:31 +0900

    링크 업데이트 live addons 날아가는 버그

--- link.cmd
+++ link.cmd
@@ -1,4 +1,5 @@
 @echo off
+rem 링크든 데이터든 심볼릭 링크를 위해 삭제
 rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\gamedata" /S /Q
 rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoKR" /S /Q
 rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoUI" /S /Q
@@ -6,9 +7,9 @@ rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DailyAlchemyKRPatch
 rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DailyProvisioningKRPatch" /S /Q
 rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DolgubonsLazyWritCreatorKRPatch" /S /Q
 rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DestinationsKRPatch" /S /Q
+rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\TamrielTradeCentreKRPatch" /S /Q
 
-rd "%userprofile%\Documents\Elder Scrolls Online\pts\AddOns\" /S /Q
-
+rem 심볼릭 링크
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\gamedata" "%cd%\gamedata"
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoKR" "%cd%\EsoKR"
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoUI" "%cd%\EsoUI"
@@ -16,5 +17,10 @@ mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DailyAlchemy
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DailyProvisioningKRPatch" "%cd%\DailyProvisioningKRPatch"
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DolgubonsLazyWritCreatorKRPatch" "%cd%\DolgubonsLazyWritCreatorKRPatch"
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DestinationsKRPatch" "%cd%\DestinationsKRPatch"
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\TamrielTradeCentreKRPatch" "%cd%\TamrielTradeCentreKRPatch"
 
+rem PTS 서버 삭제 * 마지막에 \ 붙히면 live 애드온들 다 날아감.
+rd "%userprofile%\Documents\Elder Scrolls Online\pts\AddOns" /S /Q
+
+rem PTS 서버 애드온 Live 심볼릭으로 끌어옴
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\pts\AddOns\" "%userprofile%\Documents\Elder Scrolls Online\live\AddOns"
