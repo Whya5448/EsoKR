5d781fbaf9d0bbca95639085d650a4954549fcc5
작은게이 <whya5448@gmail.com>
2019-05-16 20:51:39 +0900

    EsoUI 깃 레포지터리로 이동

--- link.cmd
+++ link.cmd
@@ -8,16 +8,18 @@ rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DailyProvisioningKR
 rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DolgubonsLazyWritCreatorKRPatch" /S /Q
 rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DestinationsKRPatch" /S /Q
 rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\TamrielTradeCentreKRPatch" /S /Q
+rd "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\CraftStoreFixedAndImprovedKRPatch" /S /Q
 
 rem 심볼릭 링크
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\gamedata" "%cd%\gamedata"
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoKR" "%cd%\EsoKR"
 mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\EsoUI" "%cd%\EsoUI"
-mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DailyAlchemyKRPatch" "%cd%\DailyAlchemyKRPatch"
-mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DailyProvisioningKRPatch" "%cd%\DailyProvisioningKRPatch"
-mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DolgubonsLazyWritCreatorKRPatch" "%cd%\DolgubonsLazyWritCreatorKRPatch"
-mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DestinationsKRPatch" "%cd%\DestinationsKRPatch"
-mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\TamrielTradeCentreKRPatch" "%cd%\TamrielTradeCentreKRPatch"
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DailyAlchemyKRPatch" "%cd%\..\DailyAlchemyKRPatch"
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DailyProvisioningKRPatch" "%cd%\..\DailyProvisioningKRPatch"
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DolgubonsLazyWritCreatorKRPatch" "%cd%\..\DolgubonsLazyWritCreatorKRPatch"
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\DestinationsKRPatch" "%cd%\..\DestinationsKRPatch"
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\TamrielTradeCentreKRPatch" "%cd%\..\TamrielTradeCentreKRPatch"
+mklink /D "%userprofile%\Documents\Elder Scrolls Online\live\AddOns\CraftStoreFixedAndImprovedKRPatch" "%cd%\..\CraftStoreFixedAndImprovedKRPatch"
 
 rem PTS 서버 삭제 * 마지막에 \ 붙히면 live 애드온들 다 날아감.
 rd "%userprofile%\Documents\Elder Scrolls Online\pts\AddOns" /S /Q
